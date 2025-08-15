899e200b6d29e178cf9d88ea3ed7e1a6a8f233a3
mahfudzalfa <mahfudzworksss@gmail.com>
2025-08-15 17:11:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// Animasi fade
+const fadeInElements = document.querySelectorAll(
+  ".fade-in-left, .fade-in-right, .fade-in-top, .fade-in-bottom, .fade-in-center"
+);
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+fadeInElements.forEach((element) => observer.observe(element));
+
+// Scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
+});
+
+// Slidder
+let currentIndex = 0;
+const items = document.querySelectorAll(".slider-item");
+const totalItems = items.length;
+
+function moveToNextSlide() {
+  currentIndex = (currentIndex + 1) % totalItems;
+  const slider = document.querySelector(".slider");
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(moveToNextSlide, 3000);
